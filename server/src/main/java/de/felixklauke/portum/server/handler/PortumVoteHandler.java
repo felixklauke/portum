68967d9c4dbb54b8c4019679320868be2e17626f
Felix Klauke <info@felix-klauke.de>
2019-06-01 15:18:25 +0200

    Add vote handler as channel inbound handler.

--- server/src/main/java/de/felixklauke/portum/server/handler/PortumVoteHandler.java
+++ server/src/main/java/de/felixklauke/portum/server/handler/PortumVoteHandler.java
@@ -1,5 +1,13 @@
 package de.felixklauke.portum.server.handler;
 
-public class PortumVoteHandler {
+import de.felixklauke.portum.protocol.model.Vote;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
 
+public class PortumVoteHandler extends SimpleChannelInboundHandler<Vote> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Vote vote) throws Exception {
+
+    }
 }
