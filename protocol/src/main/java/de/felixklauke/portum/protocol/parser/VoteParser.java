1d3ecbcf6392dab48f9582945462d2a995a8f1ed
Felix Klauke <info@felix-klauke.de>
2019-06-01 15:16:41 +0200

    Add vote parsing mechanism.

--- protocol/src/main/java/de/felixklauke/portum/protocol/parser/VoteParser.java
+++ protocol/src/main/java/de/felixklauke/portum/protocol/parser/VoteParser.java
@@ -6,7 +6,15 @@ import java.util.StringTokenizer;
 
 public class VoteParser {
 
+    /**
+     * The delimiter of the vote string.
+     */
     private static final String DELIMITER = "\n";
+
+    /**
+     * The vote integrity token to check decryption.
+     */
+    private static final String VOTE_INTEGRITY_TOKEN = "";
 
     /**
      * Parse a vote from a string.
@@ -16,7 +24,22 @@ public class VoteParser {
      */
     public static Vote parseVote(String string) {
 
+        // Create tokenizer
+        StringTokenizer tokenizer = new StringTokenizer(string, DELIMITER);
 
-        return new Vote("", "", "", "");
+        // Check vote integrity token
+        String voteToken = tokenizer.nextToken();
+        if (!voteToken.equalsIgnoreCase(VOTE_INTEGRITY_TOKEN)) {
+            throw new IllegalStateException("Failure during decryption. Couldn't confirm integrity.");
+        }
+
+        // Read service name, user name, address, timestamp
+        String serviceName = tokenizer.nextToken();
+        String userName = tokenizer.nextToken();
+        String address = tokenizer.nextToken();
+        String timestamp = tokenizer.nextToken();
+
+        // Construct vote
+        return new Vote(userName, serviceName, timestamp, address);
     }
 }
