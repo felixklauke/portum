46ec756feec611f491e301b2a5fc4315d4bd67ce
Felix Klauke <info@felix-klauke.de>
2019-06-10 09:33:08 +0200

    Add warnign on empty listeners.

--- server/src/main/java/de/felixklauke/portum/server/config/PortumServerConfig.java
+++ server/src/main/java/de/felixklauke/portum/server/config/PortumServerConfig.java
@@ -6,8 +6,11 @@ import java.security.KeyPair;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Logger;
 
 public class PortumServerConfig {
+
+    private final Logger logger = Logger.getLogger(PortumServerConfig.class.getSimpleName());
 
     private final List<VoteListener> listeners;
     private final String host;
@@ -19,6 +22,11 @@ public class PortumServerConfig {
         Objects.requireNonNull(listeners, "Listeners cannot be null.");
         Objects.requireNonNull(host, "Host cannot be null.");
         Objects.requireNonNull(keyPair, "Key Pair cannot be null.");
+
+        // Warn on empty listeners
+        if (listeners.size() == 0) {
+            logger.warning("You didn't specify any listeners at the starting. Ensure to register some later on!");
+        }
 
         this.listeners = new CopyOnWriteArrayList<>(listeners);
         this.host = host;
