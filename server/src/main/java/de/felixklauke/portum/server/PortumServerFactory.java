81fd82d82bbd7878d9cdcc9147b2460e34eb0f17
Felix Klauke <info@felix-klauke.de>
2019-06-10 09:27:16 +0200

    Don't allow null configs.

--- server/src/main/java/de/felixklauke/portum/server/PortumServerFactory.java
+++ server/src/main/java/de/felixklauke/portum/server/PortumServerFactory.java
@@ -5,9 +5,13 @@ import de.d3adspace.theresa.core.TheresaFactory;
 import de.felixklauke.portum.server.config.PortumServerConfig;
 import de.felixklauke.portum.server.module.PortumServerModule;
 
+import java.util.Objects;
+
 public class PortumServerFactory {
 
     public static PortumServer createServer(PortumServerConfig serverConfig) {
+
+        Objects.requireNonNull(serverConfig, "Config cannot be null");
 
         Theresa theresa = TheresaFactory.create(new PortumServerModule(serverConfig));
         return theresa.getInstance(PortumServer.class);
