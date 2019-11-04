3d12b82cbfaf91bf5397b20a81435c5541b1ec16
Felix Klauke <info@felix-klauke.de>
2019-11-04 09:53:49 +0100

    Use Preconditions instead of Objects.

--- server/src/main/java/com/felixklauke/portum/server/PortumServerFactory.java
+++ server/src/main/java/com/felixklauke/portum/server/PortumServerFactory.java
@@ -2,14 +2,14 @@ package com.felixklauke.portum.server;
 
 import com.felixklauke.portum.server.config.PortumServerConfig;
 import com.felixklauke.portum.server.module.PortumServerModule;
+import com.google.common.base.Preconditions;
 import de.d3adspace.theresa.core.Theresa;
 import de.d3adspace.theresa.core.TheresaFactory;
-import java.util.Objects;
 
 public class PortumServerFactory {
 
   public static PortumServer createServer(PortumServerConfig serverConfig) {
-    Objects.requireNonNull(serverConfig, "Config cannot be null");
+    Preconditions.checkNotNull(serverConfig);
 
     Theresa theresa = TheresaFactory.create(new PortumServerModule(serverConfig));
     return theresa.getInstance(PortumServer.class);
